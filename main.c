3c959896d37a5fe603fc45c6d7631b3fb027e06f
thomas <thomas@pop-os.localdomain>
2023-03-24 02:14:05 +0100

    sanity checks

--- main.c
+++ main.c
@@ -1,42 +1,31 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include "elf.h"
+#include "exec.h"
 
-#include "scanner.h"
-#include "ast.h"
-#include "commands.h"
-#include "parser.h"
-
-/** Main function. Calls readInputLine and calls functions to build tokenList and to build abstract syntax tree
- */
-
-int main(int argc, char *argv[]) {
-    char *inputLine;
-    List tokenList;
-    setbuf(stdin, NULL);
-    setbuf(stdout, NULL);
-
-    while (true) {
-        inputLine = readInputLine();
-        if (inputLine == NULL) {
-            break;
-        }
-        tokenList = getTokenList(inputLine);
-        List tokenListCopy = tokenList;
-        int status;
-        struct ast *tree = parseSemiLine(&tokenListCopy, &status);
-        int command;
-        if (status != 0) {
-            command = executeTree(tree);
-       } else {
-            printf("Error: invalid syntax!\n");
-        }
-        free(inputLine);
-        freeTokenList(tokenList);
-        freeSyntaxTree(tree);
-        if (command == 0) {
-            break;
-        }
+int main(int argc, char *argv[])
+{
+    struct elf_header *elf_head;
+    if (argc != 2) {
+        printf("Usage: %s <binary> [arguments] \n", argv[0]);
+        exit(1);
     }
+    FILE* ptr;
+    ptr = fopen(argv[1], "rb");
+
+    if (ptr == NULL) {
+        printf("Error in opening file %s\n", argv[1]);
+        exit(1);
+    }
+    fread(elf_head, sizeof(struct elf_header), 1, ptr);
+
+    if (elf_head->magic != ELF_MAGIC ||
+        elf_head->machine != ELF_MACHINE_x64 ||
+        elf_head->version != ELF_VERSION) {
+        printf("Invalid ELF file!\n");
+        exit(1);
+    }
+    
+    fclose(ptr);
     return 0;
 }
